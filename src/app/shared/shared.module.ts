dbbe0c595216691463267c218f394d1dd3455d20
vamseeuxd <vamsee.uxd@gmail.com>
2020-06-08 22:23:40 +0530

    added dynamic form component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DynamicFormComponent} from './components/dynamic-form/dynamic-form.component';
-import {MinDirective} from './directive/min.directive';
-import {MaxDirective} from './directive/max.directive';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
+import { MinDirective } from "./directive/min.directive";
+import { MaxDirective } from "./directive/max.directive";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [DynamicFormComponent, MinDirective, MaxDirective],
@@ -21,5 +21,4 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatCheckboxModule,
   ],
 })
-export class SharedModule {
-}
+export class SharedModule {}
